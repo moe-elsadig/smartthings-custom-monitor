b1f61c5f214d1218eef3869114f1ba366941b433
moe-elsadig <m.da7th@gmail.com>
2023-11-14 11:33:33 +0300

    change export to CSV

--- src/components/ExportTable.js
+++ src/components/ExportTable.js
@@ -1,49 +1,58 @@
 import { Button } from "@mui/material";
 import axios from "axios";
 import React from "react";
-import * as XLSX from "xlsx";
+import * as Papa from "papaparse";
 
 const ExportTable = ({ data, columns }) => {
     function getValueByPath(obj, path) {
         return path.split(".").reduce((acc, key) => acc && acc[key], obj);
     }
 
-    // console.log("columns", columns);
-    // console.log("data", data);
-
     const handleExportClick = () => {
         let dataRecords = [];
         axios
             .get("/data")
             .then((res) => {
-                // console.log(res.data.length);
                 dataRecords = res.data.dataRecords;
             })
             .then(() => {
-                const wb = XLSX.utils.book_new();
-
                 // Extract data for selected columns
                 const exportData = dataRecords.map((item) => {
                     return columns.map((col) => getValueByPath(item, col));
                 });
 
-                // console.log("exportData", exportData);
                 // Add headers to the data
                 let headers = columns.map((col) =>
                     col.replace("components.main.", "")
                 );
-                const wsData = [headers, ...exportData];
+                const csvData = [headers, ...exportData];
 
-                const ws = XLSX.utils.aoa_to_sheet(wsData);
-                XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
-                XLSX.writeFile(wb, "exported_table.xlsx");
+                // Convert data to CSV format
+                const csv = Papa.unparse(csvData);
+
+                // Create a Blob containing the CSV data
+                const blob = new Blob([csv], {
+                    type: "text/csv;charset=utf-8;",
+                });
+
+                // Create a download link and trigger a click event to download the CSV file
+                const link = document.createElement("a");
+                if (link.download !== undefined) {
+                    const url = URL.createObjectURL(blob);
+                    link.setAttribute("href", url);
+                    link.setAttribute("download", "exported_table.csv");
+                    link.style.visibility = "hidden";
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                }
             });
     };
 
     return (
         <div>
             <Button variant="contained" onClick={handleExportClick}>
-                Export to Excel
+                Export to CSV
             </Button>
         </div>
     );
