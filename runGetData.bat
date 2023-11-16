959d5b2ef630817026301a0a05879b1b6eac815e
moe-elsadig <m.da7th@gmail.com>
2023-11-16 14:50:21 +0300

    add individual launchers

---
+++ runGetData.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Check if ST-monitor-server is running
+tasklist /FI "WINDOWTITLE eq ST-monitor-server*" | find "cmd.exe" > nul
+if errorlevel 1 (
+    start "ST-monitor-server" cmd /k "node .\data_service\getData.js"
+)
